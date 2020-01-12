c2e44eb2fed9205ee934bc47b4b24bd020d9756a
Ho Yin Keung <hykeung92@gmail.com>
2020-01-12 23:28:42 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,8 +52,7 @@ class Owner
   
   def sell_pets
     pets = self.dogs + self. cats
-    pets.each {|pet| pet.mood = "nervous"}
-    pets
+    pets.each {|pet| pet.mood = "nervous", pet.owner = "nil"}
   end
     
     
